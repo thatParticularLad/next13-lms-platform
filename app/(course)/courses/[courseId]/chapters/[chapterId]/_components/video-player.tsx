384d0b66d3ff4a9ac702504769e9f72243250ee1
Adomas <sadasdasdas2312@gmail.com>
2024-03-08 07:27:07 +0200

    update accent color

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -78,6 +78,7 @@ export const VideoPlayer = ({
           className={cn(!isReady && "hidden")}
           onCanPlay={() => setIsReady(true)}
           onEnded={onEnd}
+          accentColor="#01577A"
           playbackId={playbackId}
         />
       )}
