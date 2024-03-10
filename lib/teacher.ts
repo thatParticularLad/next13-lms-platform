75aab905c78490c7538073f929dc261c3e369b7f
Adomas <sadasdasdas2312@gmail.com>
2024-03-10 17:00:10 +0200

    make course creation path private

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,9 +1,9 @@
 // Uncomment in PROD
 
-// export const isTeacher = (userId?: string | null) => {
-//   return userId === process.env.NEXT_PUBLIC_TEACHER_ID;
-// }
-
 export const isTeacher = (userId?: string | null) => {
-  return !!userId;
+  return userId === process.env.NEXT_PUBLIC_TEACHER_ID;
 };
+
+// export const isTeacher = (userId?: string | null) => {
+//   return !!userId;
+// };
